5ebc9664f7d20a1119ba04646309048e284976d3
sergey <baraulia@yandex.ru>
2021-11-19 16:58:58 +0300

    SumsMatrix

--- task1.go
+++ task1.go
@@ -16,6 +16,38 @@ func checkPalindrome(s string) {
 	}
 }
 
+//function sums the elements of the matrix
+func matrixSum(m [][]int) int {
+	var sum int
+	var index = make(map[int]string)
+	for i := range m {
+		switch {
+		case i == 0:
+			for n, a := range m[0] {
+				index[n] = ""
+				sum += a
+				if a == 0 {
+					delete(index, n)
+				}
+			}
+		case i != 0:
+			if len(m[0]) != len(m[i]) {
+				fmt.Println("Передайте срез срезов с одинаковой длиной!")
+			} else {
+				for n := range index {
+					if m[i][n] == 0 {
+						delete(index, n)
+					} else {
+						sum += m[i][n]
+					}
+
+				}
+			}
+		}
+	}
+	return sum
+}
+
 func main() {
 	str1 := "aabaa"
 	str2 := "abac"
@@ -23,4 +55,15 @@ func main() {
 	checkPalindrome(str1)
 	checkPalindrome(str2)
 	checkPalindrome(str3)
+	//________________________________________________
+	matrix := [][]int{{0, 2, 3, 4},
+		{0, 0, 0, 1},
+		{1, 1, 4, 8},
+	}
+	matrix2 := [][]int{{1, 1, 1, 0},
+		{0, 5, 0, 1},
+		{2, 1, 3, 10}}
+	fmt.Println(matrixSum(matrix))
+	fmt.Println(matrixSum(matrix2))
+	//________________________________________________
 }
